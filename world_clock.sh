35c2cf6cde578c174fb6400e1bf1dde616a64b32
ttiimmothy <dinosauli2006@gmail.com>
2024-04-22 00:46:48 -0400

    feat update world clock script

--- world_clock.sh
+++ world_clock.sh
@@ -4,7 +4,7 @@
 # @raycast.schemaVersion 1
 # @raycast.title World clock
 # @raycast.mode inline
-# @raycast.refreshTime 5s
+# @raycast.refreshTime 3s
 
 # Optional parameters:
 # @raycast.packageName Clock
@@ -18,11 +18,13 @@
 
 # Timezones can be found in /usr/share/zoneinfo
 
+los_angeles=$(TZ=America/Los_Angeles date +"%H:%M")
 vancouver=$(TZ=America/Vancouver date +"%H:%M")
-calgary=$(TZ=America/Edmonton date +"%H:%M")
+edmonton=$(TZ=America/Edmonton date +"%H:%M")
 toronto=$(TZ=America/Toronto date +"%H:%M")
 hong_kong=$(TZ=Asia/Hong_Kong date +"%H:%M")
 tokyo=$(TZ=Asia/Tokyo date +"%H:%M")
+melbourne=$(TZ=Australia/Melbourne date +"%H:%M")
 
-# echo "Vancouver, Los Angeles: $vancouver | Calgary: $calgary | Toronto: $toronto | Hong Kong: $hong_kong | Tokyo: $tokyo"
-echo "Vancouver: $vancouver | Tokyo: $tokyo"
+# echo "Los Angeles, Vancouver : $los_angeles | Edmonton: $edmonton | Toronto: $toronto | Hong Kong: $hong_kong | Tokyo: $tokyo | Melbourne: $melbourne"
+echo "Vancouver: $los_angeles | Edmonton: $edmonton | Tokyo: $tokyo"
