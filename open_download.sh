9e2bdd316548f2aab9f07d7853688dc644c6ab3a
ttiimmothy <dinosauli2006@gmail.com>
2024-04-22 04:35:32 -0400

    feat add finder related script command (downloads, desktop, repository folders)

---
+++ open_download.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Download
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName Finder
+# @raycast.icon icons/finder_icon.png
+
+# Documentation:
+# @raycast.description open download folder
+# @raycast.author timoptimothy
+# @raycast.authorURL https://raycast.com/timoptimothy
+
+open ~/Downloads
---
+++ open_repository.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Repository
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName Finder
+# @raycast.icon icons/finder_icon.png
+
+# Documentation:
+# @raycast.description open github repository folder
+# @raycast.author timoptimothy
+# @raycast.authorURL https://raycast.com/timoptimothy
+
+open ~/Developer/github_repository
