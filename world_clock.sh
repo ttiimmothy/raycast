ca7e3709b4066c94a578937cd9cb42aa2d1dd634
ttiimmothy <dinosauli2006@gmail.com>
2024-04-24 06:31:22 -0400

    feat update world clock script command

--- world_clock.sh
+++ world_clock.sh
@@ -18,6 +18,10 @@
 
 # Timezones can be found in /usr/share/zoneinfo
 
+green="\033[0;32m"
+blue="\033[1;34m"
+cyan="\033[0;36m"
+no_color='\033[0m'
 los_angeles=$(TZ=America/Los_Angeles date +"%H:%M")
 vancouver=$(TZ=America/Vancouver date +"%H:%M")
 edmonton=$(TZ=America/Edmonton date +"%H:%M")
@@ -26,5 +30,5 @@ hong_kong=$(TZ=Asia/Hong_Kong date +"%H:%M")
 tokyo=$(TZ=Asia/Tokyo date +"%H:%M")
 melbourne=$(TZ=Australia/Melbourne date +"%H:%M")
 
-# echo "Los Angeles, Vancouver : $los_angeles | Edmonton: $edmonton | Toronto: $toronto | Hong Kong: $hong_kong | Tokyo: $tokyo | Melbourne: $melbourne"
-echo "Vancouver: $los_angeles | Edmonton: $edmonton | Tokyo: $tokyo"
+# echo -e "Los Angeles: $los_angeles ${green}|${no_color} Vancouver: $vancouver ${green}|${no_color} Edmonton: $edmonton ${green}|${no_color} Toronto: $toronto ${green}|${no_color} Hong Kong: $hong_kong ${green}|${no_color} Tokyo: $tokyo ${green}|${no_color} Melbourne: $melbourne"
+echo -e "Vancouver: $vancouver ${blue}|${no_color} Edmonton: $edmonton ${blue}|${no_color} Tokyo: $tokyo"
