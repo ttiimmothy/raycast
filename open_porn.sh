4a49095d902c1c59a591c6b297f7c0e00c4574b1
ttiimmothy <dinosauli2006@gmail.com>
2024-05-12 19:06:12 -0400

    feat add open porn folder script command

---
+++ open_porn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Porn
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName Finder
+# @raycast.icon icons/finder_icon.png
+
+# Documentation:
+# @raycast.description open porn folder
+# @raycast.author timoptimothy
+# @raycast.authorURL https://raycast.com/timoptimothy
+
+open ~/Integration/porn
