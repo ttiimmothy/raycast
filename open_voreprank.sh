351235819d928bfde658b58f1460e6aca5b88095
ttiimmothy <dinosauli2006@gmail.com>
2024-08-20 23:24:37 -0400

    feat add open voreprank folder script

---
+++ open_voreprank.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Voreprank
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName Finder
+# @raycast.icon icons/finder_icon.png
+
+# Documentation:
+# @raycast.description open voreprank folder
+# @raycast.author timoptimothy
+# @raycast.authorURL https://raycast.com/timoptimothy
+
+open ~/Integration/voreprank
