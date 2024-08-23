0c83486efd1b1b099f5e03a857f9927290dddae2
ttiimmothy <dinosauli2006@gmail.com>
2024-08-23 17:37:54 -0400

    feat add plusout folder script

---
+++ open_plusout.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Plusout
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName Finder
+# @raycast.icon icons/finder_icon.png
+
+# Documentation:
+# @raycast.description open plusout folder
+# @raycast.author timoptimothy
+# @raycast.authorURL https://raycast.com/timoptimothy
+
+open ~/Integration/Plusout
